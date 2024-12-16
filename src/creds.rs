6c242f266cb2658e48f172ef3e7a88d858c4ca2f
Fake.User <russellboswell@protonmail.com>
2024-12-15 23:37:57 -0500

    order

--- src/creds.rs
+++ src/creds.rs
@@ -8,9 +8,9 @@ use std::env;
 
 #[derive(Deserialize, Serialize)]
 pub struct Credentials {
-    access_key_id: String,
     secret_access_key: String,
-    session_token: String,
+    access_key_id: String,
+    session_token: String
 }
 
 #[derive(Deserialize)]
@@ -20,21 +20,21 @@ struct CloudflareResponse {
 
 #[derive(Deserialize)]
 struct CloudflareResult {
-    #[serde(rename = "accessKeyId")]
-    access_key_id: String,
     #[serde(rename = "secretAccessKey")]
     secret_access_key: String,
+    #[serde(rename = "accessKeyId")]
+    access_key_id: String,
     #[serde(rename = "sessionToken")]
-    session_token: String,
+    session_token: String
 }
 
 pub async fn get_creds() -> Result<Json<Credentials>, StatusCode> {
     let client = Client::new();
 
     let body = serde_json::json!({
-        "bucket": "destruct-data",
         "parentAccessKeyId": env::var("AWS_ACCESS_KEY_ID").unwrap(),
         "permission": "object-read-only",
+        "bucket": "destruct-data",
         "ttlSeconds": 3600
     });
 
@@ -43,9 +43,10 @@ pub async fn get_creds() -> Result<Json<Credentials>, StatusCode> {
             "https://api.cloudflare.com/client/v4/accounts/{}/r2/temp-access-credentials",
             env::var("ACCOUNT_ID").unwrap()
         ))
-        .header("Content-Type", "application/json")
+
         .header("X-Auth-Email", env::var("X_AUTH_EMAIL").unwrap())
         .header("X-Auth-Key", env::var("X_AUTH_KEY").unwrap())
+        .header("Content-Type", "application/json")
         .json(&body)
         .send()
         .await
@@ -57,8 +58,8 @@ pub async fn get_creds() -> Result<Json<Credentials>, StatusCode> {
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     let creds = Credentials {
-        access_key_id: cloudflare_response.result.access_key_id,
         secret_access_key: cloudflare_response.result.secret_access_key,
+        access_key_id: cloudflare_response.result.access_key_id,
         session_token: cloudflare_response.result.session_token,
     };
 
