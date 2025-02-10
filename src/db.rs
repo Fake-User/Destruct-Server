e4b28a7de19d67a8d587b0205270845dbe436e80
Fake.User <russellboswell@protonmail.com>
2025-02-09 23:07:03 -0500

    stream

--- src/db.rs
+++ src/db.rs
@@ -1,41 +1,39 @@
-use futures::stream::Stream;
+use futures::{stream, stream::{Stream, StreamExt}};
 use std::sync::Arc;
-use std::{fs, env};
+use std::io::Write;
 use axum::{
-    response::{Sse, IntoResponse},
-    response::sse::Event,
+    response::{sse::Event, Sse, IntoResponse},
     http::HeaderMap,
     extract::State
 };
 use crate::AppState;
-use futures::stream;
 
-pub async fn get_db(State(state): State<Arc<AppState>>) -> Sse<impl Stream<Item = Result<Event, std::io::Error>> + Send + 'static>{
-    let rx = state.db_notifier.subscribe();
+pub async fn get_db(
+    State(state): State<Arc<AppState>>, uri: axum::http::Uri) -> Sse<impl Stream<Item = Result<Event, std::io::Error>> + Send> {
+    let client_utc = uri.query().unwrap_or("").replace("%20", " ").to_string();
+    let rx = state.db_update.subscribe();
+    let initial = if client_utc < *state.db_utc.read().unwrap(){Some(Ok(Event::default().data(rx.borrow().clone())))}
+    else{None};
 
-    let stream = stream::unfold(rx, |mut rx| async move{
-        let data = rx.borrow().clone();
-        let event = Event::default().data(data);
-        let result = Ok(event);
-
+    let stream = futures::stream::iter(initial).chain(stream::unfold(rx, |mut rx| async move{
         match rx.changed().await{
-            Ok(()) => Some((result, rx)),
+            Ok(()) => {
+                let data = rx.borrow().clone();
+                Some((Ok(Event::default().data(data)), rx))
+            }
             Err(_) => None,
         }
-    });
+    }));
 
     Sse::new(stream)
 }
 
 pub async fn set_db(State(state): State<Arc<AppState>>, headers: HeaderMap, body: String) -> impl IntoResponse{
-    if headers.get("authorization").and_then(|h| h.to_str().ok()) != env::var("UTC_KEY").ok().as_deref(){
-        return "authorization error".into_response()
-    };
-
-    if let Err(_) = fs::write("/store/db-data.js", &body){
-        return "error writing db-data.js".into_response()
-    };
-
-    let _ = state.db_notifier.send(body);
+    if headers.get("authorization").and_then(|h| h.to_str().ok()) != std::env::var("UTC_KEY").ok().as_deref(){return "authorization error".into_response()};
+    let utc = body.clone().split_once('"').unwrap().1.split_once('"').unwrap().0.to_string();
+    *state.db_utc.write().unwrap() = utc;
+    let mut file = std::fs::File::create("store/db-data.js").unwrap();
+    file.write_all(body.as_bytes()).unwrap();
+    let _ = state.db_update.send(body);
     "db updated".into_response()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
+#[cfg(debug_assertions)]
+use dotenv::dotenv;
+
 use tower_http::cors::{CorsLayer, Any};
 use tokio::sync::watch;
 use std::sync::Arc;
 use axum::{
-    routing::get,
+    routing::{get, put},
     Router,
 };
-
-#[cfg(debug_assertions)]
-use dotenv::dotenv;
-
 mod creds;
 mod db;
 
 pub struct AppState{
-    db_notifier: watch::Sender<String>,
+    db_update: watch::Sender<String>,
+    db_utc:  std::sync::RwLock<String>
 }
 
 #[tokio::main]
@@ -22,12 +22,13 @@ async fn main(){
     dotenv().ok();
 
     let (tx, _rx) = watch::channel(String::new());
-    let state = Arc::new(AppState{db_notifier: tx});
+    let state = Arc::new(AppState{db_update: tx, db_utc: std::sync::RwLock::new("".to_string())});
 
-    // Load initial data if it exists
-    if let Ok(data) = std::fs::read_to_string("./store/db-data.js"){
-        let _ = state.db_notifier.send(data);
-    }
+    if let Ok(data) = std::fs::read_to_string("store/db-data.js"){
+        let utc = data.clone().split_once('"').unwrap().1.split_once('"').unwrap().0.to_string();
+        *state.db_utc.write().unwrap() = utc;
+        state.db_update.send(data).unwrap();
+    };
 
     let cors = CorsLayer::new()
         .allow_methods(Any)
@@ -36,7 +37,7 @@ async fn main(){
 
     let app = Router::new()
         .route("/get-db", get(db::get_db))
-        .route("/set-db", get(db::set_db))
+        .route("/set-db", put(db::set_db))
         .route("/creds", get(creds::creds))
         .with_state(state)
         .layer(cors);
